923c95a506ffdb2f5756a30aa2a6b50ddf7aeadb
John Walter <ashthecaver@gmail.com>
2025-09-03 19:18:29 -0400

    In put in some rotations and slowed the speed down.

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    xgo.move_xgo(xgo.direction_enum.Forward, 75)
-    xgo.execution_action(xgo.action_enum.Go_prone)
+    xgo.move_xgo(xgo.direction_enum.Forward, 47)
+    xgo.translational_motion_reciprocate_stop(xgo.body_direction_xyz_enum.X)
+    xgo.rotate(xgo.rotate_enum.Left, 25)
+    xgo.rotate(xgo.rotate_enum.Left, 24)
+    xgo.execution_action(xgo.action_enum.Squat)
     xgo.move_xgo(xgo.direction_enum.Forward, 75)
 })
 xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
