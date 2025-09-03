f27a81cebc191ec5d2ea8c9d95198f4fdcbf4e53
John Walter <ashthecaver@gmail.com>
2025-09-03 19:05:13 -0400

    Trying to get demon doogo to move with Microbit

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
-xgo.execution_action(xgo.action_enum.Go_prone)
-xgo.move_xgo(xgo.direction_enum.Forward, 75)
-xgo.rotate_angle(xgo.rotate_direction_enum.turn_right, 74)
-xgo.move_xgo(xgo.direction_enum.Forward, 75)
-xgo.leg_lift_continue(0, 0)
+input.onButtonPressed(Button.A, function () {
+    xgo.execution_action(xgo.action_enum.Go_prone)
+    xgo.move_xgo(xgo.direction_enum.Forward, 75)
+    xgo.rotate_angle(xgo.rotate_direction_enum.turn_right, 74)
+    xgo.move_xgo(xgo.direction_enum.Forward, 75)
+    xgo.leg_lift_continue(0, 0)
+})
+xgo.init_xgo_serial(SerialPin.P14, SerialPin.P13)
